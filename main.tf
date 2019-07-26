1e43dfe331b0648d05846d787594edd8814704d3
Dan Siwiec <daniel.siwiec@gmail.com>
2019-07-26 15:26:49 -0700

    Add postgresql to terraform

--- main.tf
+++ main.tf
@@ -43,3 +43,14 @@ resource "helm_release" "publisher" {
     value = "sfo/python-publisher"
   }
 }
+
+resource "helm_release" "pg" {
+  name       = "pg"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
+  chart      = "postgresql"
+  namespace  = "kafka"
+
+  values = [
+    "${file("pg/pg-values.yaml")}"
+  ]
+}
