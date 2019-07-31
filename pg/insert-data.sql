f93a74678b831f5fa32506672d503b98a06826d1
Dan Siwiec <daniel.siwiec@gmail.com>
2019-07-31 15:36:39 -0700

    Remove unused files

--- pg/insert-data.sql
+++
@@ -1,9 +0,0 @@
---select * from people;
-
-insert into people (first, last) values('akbar', 'pasha');
-
-insert into people (first, last) values('akbar2', 'pasha2');
-
-insert into people (first, last) values('akbar3', 'pasha3');
-
-insert into people (first, last) values('akbar4', 'pasha4');
